cfcad866a02c863f2ebd3a7f32da148862b39f14
Abdul Wahab Asghar <abzz1290@gmail.com>
2023-06-16 20:47:53 +0500

    Displaying platform icon

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -2,10 +2,17 @@ import { useState, useEffect, SetStateAction } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -24,10 +31,10 @@ const useGames = () => {
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
-      .catch((err) =>{
-      if (err instanceof CanceledError) return;
-      setError(err.message)
-  });
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
 
     return () => controller.abort();
   }, []);
